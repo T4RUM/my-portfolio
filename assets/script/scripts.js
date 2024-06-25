e328f79630eb1d97616ed4d4230f1551eba891c6
T4RUM <wesley-murat@hotmail.com>
2024-06-24 20:54:02 -0700

    Corrigindo bug para atualizar hora automaticamente com a função setInterval

--- assets/script/scripts.js
+++ assets/script/scripts.js
@@ -1,8 +1,12 @@
 // Pegando a hora atual
-const horas = document.querySelector('.time');
-const data = new Date();
-let hora = data.getHours();
-let minutos = data.getMinutes().toString().padStart(2, '0');
+function atualizarHora() {
+  const horas = document.querySelector(".time");
+  const data = new Date();
+  let hora = data.getHours().toString().padStart(2, "0");
+  let minutos = data.getMinutes().toString().padStart(2, "0");
 
-// Usando template literals para definir o conteúdo do elemento
-horas.innerHTML = `${hora}:${minutos}`;
+  horas.innerHTML = `${hora}:${minutos}`;
+}
+
+// Atualiza a hora a cada segundo
+const intervalID = setInterval(atualizarHora, 1000);
